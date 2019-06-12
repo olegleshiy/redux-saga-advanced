42f2f3cb0b1c016b200d2e0b3a501cb3eed4acae
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-06-12 13:37:59 +0300

    refactor(source): upgrade code examples

--- src/examples/1/example.js
+++ src/examples/1/example.js
@@ -1,15 +1,15 @@
 /**
- * Основная концепция инструкций в redux-saga — это эффекты.
- * Эффект — это JavaScript объект, описывающий инструкцию.
- * take, call, put, apply и др. — это фабрики, производящие эффекты для разных целей.
+ * В саге-генераторе можно вызывать нужные методы в явном виде, без использования эффектов.
+ * Можно также комбинировать эффекты с явными операциями без эффектов.
  *
- * Сага-генератор предает эффект (или результат выражения справа от yield) middleware-у,
- * который и выполняет основную логику, описанную эффектом (или выражением).
- * После выполнения операции middleware «пробрасывает» результат обратно внутрь генератора.
+ * Однако для описаний практически любой инструкции в
+ * Redux Saga существует специальный эффект.
+ * Примеры эффектов в этом файле — take и put.
+ * put — самый простой эффект, он работает как store.dispatch.
  */
 
 // Core
-import { take, call, apply, put } from 'redux-saga/effects';
+import { take, put } from 'redux-saga/effects';
 
 // Instruments
 import { types } from '../../bus/swapi/types';
@@ -21,8 +21,8 @@ export function* runExample() {
         const action = yield take(types.FETCH_PLANETS_ASYNC);
 
         yield put(swapiActions.setIsFetching(true));
-        const response = yield call(api.fetchPlanets, [ action.payload ]);
-        const data = yield apply(response, response.json);
+        const response = yield api.fetchPlanets(action.payload);
+        const data = yield response.json();
 
         yield put(swapiActions.fillPlanets(data.results));
         yield put(swapiActions.setIsFetching(false));
